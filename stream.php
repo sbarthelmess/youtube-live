8946d446b951272d88a75768ff4614a3d91aafb3
Sebastian Barthelmess <seb@latestlinux.com>
2022-05-02 19:31:04 -0400

    An example server-side script for updating stream URL
    I use a Freehostia website, which allows for PHP, this just lets me easily write a stream ID to the webserver.  Then I can read that file in javascript so my URL always remains active.  Thanks YouTube for making this difficult!   Accepts value by post or by URL param.

---
+++ stream.php
@@ -0,0 +1,25 @@
+<?php
+  $stream_id = 0;
+  if(isset($_GET['stream_id'])) $stream_id=trim($_GET['stream_id']); 
+  if(isset($_POST['text_box'])) $stream_id=trim($_POST['text_box']);
+  
+  // Do some string validation
+  if (strlen($stream_id)==11) {
+          $myFile = "stream.id";
+          $fh = fopen($myFile, 'w') or die("can't open file");
+          fwrite($fh, $a);
+          fclose($fh);
+          $SUCCESS=true;
+          if (isset($_GET['stream_id'])) exit("SUCCESS, url updated: $stream_id\n");
+  }
+?>
+<html>
+    <body>
+        <h1><?php if ($SUCCESS) echo "Successfully updated: $stream_id";?>
+        <form name="form" method="post">
+            <input type="text" name="text_box" size="15"/>
+            <input type="submit" id="search-submit" value="submit" />
+        </form>
+        </h1>
+    </body>
+</html>
